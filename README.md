c71e177fbb93cd1d692d5acdd2949d60b4af3c55
Cristian Alberto Vega Sanhueza <Cristian.Vega@sonda.com>
2024-09-02 12:30:52 -0400

    Proyecto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafío 04 - Pizzería Mamma Mía Consumo de APIs con React
+
+Deploy: https://cvegasan.github.io/pizzaMamaMiaHito04-react/
---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Home from './Pages/Home';
+
+// Componentes LOGIN y REGISTRO
+import Login from './Pages/FormLogin';
+import Register from './Pages/FormRegistro';
+import Pizza from './Pages/Pizza';
+import Profile from './Pages/Profile';
+import NotFound from './Pages/NotFound';
+import Cart from './Pages/Cart';
+
+//React Router DOM
+import { BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    //App.jsx se mostrarán los componentes Navbar, Home y Footer
+
+  <BrowserRouter>
+        <NavBar></NavBar>
+
+        <Routes>
+          <Route path="/pizzaMamaMiaHito05-react" element={<Home/>} />
+          <Route path="/pizzaMamaMiaHito05-react/register" element={<Register/>} />
+          <Route path="/pizzaMamaMiaHito05-react/login" element={<Login/>} />
+          <Route path="/pizzaMamaMiaHito05-react/pizza/p001" element={<Pizza/>} />
+          <Route path="/pizzaMamaMiaHito05-react/profile" element={<Profile/>} />
+          <Route path="/pizzaMamaMiaHito05-react/cart" element={<Cart/>} />
+
+          <Route path='/pizzaMamaMiaHito05-react/404' element={<NotFound/>} />
+          <Route path='*' element={<Navigate replace to='/pizzaMamaMiaHito05-react/404'/>} />
+        </Routes>
+
+
+        {/* <Cart/> */}
+        {/* <Pizza/> */}
+        <Footer></Footer>
+  </BrowserRouter>
+  )
+}
+export default App;
